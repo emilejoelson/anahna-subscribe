ac196e7c46a3eae158b3c5dc534d1566c3c670db
cunns <youssef.seqally@gmail.com>
2025-04-16 12:20:48 +0100

    ccc

--- backend-api/src/types.ts
+++
@@ -1,9 +0,0 @@
-import { PubSub } from 'graphql-subscriptions';
-import { Request, Response } from 'express';
-
-export interface GraphQLContext {
-  pubsub: PubSub;
-  req?: Request;
-  res?: Response;
-  // Add any other context properties here (auth, etc.)
-}
