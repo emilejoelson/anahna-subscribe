68439bf1c1c48edc6aa09efb3cbbdf7e6b621517
cunns <youssef.seqally@gmail.com>
2025-04-22 11:52:00 +0100

    Revet changes in commit

---
+++ backend-api/models/category.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const categorySchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  description: String,
+  restaurant: {
+    type: Schema.Types.ObjectId,
+    ref: 'Restaurant',
+    required: true
+  },
+  foods: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Food'
+  }],
+  subCategories: [{
+    title: {
+      type: String,
+      required: true
+    },
+    description: String,
+    isActive: {
+      type: Boolean,
+      default: true
+    },
+    parentCategoryId: {
+      type: Schema.Types.ObjectId,
+      ref: 'Category'
+    }
+  }],
+  image: {
+    type: String
+  },
+  isActive: {
+    type: Boolean,
+    default: true
+  }
+}, {
+  timestamps: true
+});
+
+module.exports = mongoose.model('Category', categorySchema);
