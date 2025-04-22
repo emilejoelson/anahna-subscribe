9e8b579a382a5fea768b3aa8a3eb57b00ddab137
ANDRIAMIHAJA Joeson Emile <122634018+emilejoelson@users.noreply.github.com>
2025-04-22 10:44:04 +0100

    fix ..

--- backend-api/middleware/error-handler.js
+++
@@ -1,9 +0,0 @@
-const errorHandler = (err, req, res, next) => {
-  console.error('Error:', err.stack);
-  res.status(500).json({
-    message: 'Internal server error',
-    ...(process.env.NODE_ENV === 'development' && { details: err.message })
-  });
-};
-
-module.exports = errorHandler;
